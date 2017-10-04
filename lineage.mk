b6fadf2b14f1b3e9729b10f7259bf4ab47223bc8
Abidur Rahman Abid <30568347+nxtabid@users.noreply.github.com>
2017-10-04 14:59:13 +0600

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -6,6 +6,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := v3702
 PRODUCT_NAME := lineage_v3702
-PRODUCT_BRAND := BQ
-PRODUCT_MODEL := BQS-5020
-PRODUCT_MANUFACTURER := BQ
+PRODUCT_BRAND := WALTON
+PRODUCT_MODEL := Primo_GH6
+PRODUCT_MANUFACTURER := WALTON
